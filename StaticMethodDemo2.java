a8af57ecf15e6aa0dba97329d7c41bf2a1d55d83
SHIVAM VERMA <SHIVAMKVERMA@users.noreply.github.com>
2019-09-16 15:19:16 +0530

    Static Method Interface
    Added File(s): StaticMethodDemo2.java
    Author: Shivam Verma

---
+++ StaticMethodDemo2.java
@@ -0,0 +1,11 @@
+interface Interf {
+	public static void main(String[] args) {
+		System.out.println("interface Main Method");	
+	}
+
+}
+
+public class StaticMethodDemo1 implements Interf {
+	
+
+}
